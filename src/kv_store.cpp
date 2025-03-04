8b20bf87eed04c8c6d036aa3fd664f53d3be719f
blacknand <nblackburndeveloper@icloud.com>
2025-03-04 14:56:42 +0000

    implement: put store remove in KVStore

--- src/kv_store.cpp
+++ src/kv_store.cpp
@@ -3,5 +3,18 @@
 KVStore::KVStore() {}
 
 void KVStore::put(const std::string &key, const std::string &value) {
-    
+    std::lock_guard<std::mutex> lock(store_mutex);
+    store[key] = value;
+}
+
+
+std::string KVStore::get(const std::string& key) {
+    std::lock_guard<std::mutex> lock(store_mutex);
+    auto it = store.find(key);
+    return (it != store.end()) ? it->second : "";
+}
+
+void KVStore::remove(const std::string& key) {
+    std::lock_guard<std::mutex> lock(store_mutex);
+    store.erase(key);
 }
