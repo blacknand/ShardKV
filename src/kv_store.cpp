805ae281b76b9cb24308d1d3187105b7404e3ca3
blacknand <nblackburndeveloper@icloud.com>
2025-03-04 16:09:22 +0000

    add kv_store

--- src/kv_store.cpp
+++ src/kv_store.cpp
@@ -1,6 +1,8 @@
 #include "kv_store.h"
 
+
 KVStore::KVStore() {}
+
 
 void KVStore::put(const std::string &key, const std::string &value) {
     std::lock_guard<std::mutex> lock(store_mutex);
@@ -13,6 +15,7 @@ std::string KVStore::get(const std::string& key) {
     auto it = store.find(key);
     return (it != store.end()) ? it->second : "";
 }
+
 
 void KVStore::remove(const std::string& key) {
     std::lock_guard<std::mutex> lock(store_mutex);
